dbdb13abe562a4d11e68a99d96e2dd8726c37554
Varun <varunsonava@gmail.com>
2018-03-24 11:10:01 +0530

    modified main activity

--- app/src/main/java/com/work/of/chaostools/careaid/MainActivity.java
+++ app/src/main/java/com/work/of/chaostools/careaid/MainActivity.java
@@ -1,13 +1,43 @@
 package com.work.of.chaostools.careaid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.app_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.menu_new_content_twitter){
+            // do something
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+        Button button= findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,configureButton.class));
+            }
+        });
     }
 }
